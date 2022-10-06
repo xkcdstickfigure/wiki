1b05ed030ce2ba19ae8ba350eefb54ba236e35b4
zero <zero@alles.cx>
2022-10-06 13:10:17 +0100

    changed internal link parsing

--- markup/link.go
+++ markup/link.go
@@ -19,37 +19,24 @@ func parseLink(link string) (bool, []string, error) {
 		return false, []string{}, errors.New("link must not have more than 2 parts (display and target)")
 	}
 
-	if strings.Contains(target, "://") {
+	if strings.Contains(target, "/") {
 		// external url
 		return true, []string{display, target}, nil
 	} else {
 		// internal link
-		var site string
 		var page string
 		var section string
 
-		// ...#section
 		split2 := strings.Split(target, "#")
-		if len(split2) == 2 {
+		if len(split2) == 1 {
+			page = target
+		} else if len(split2) == 2 {
+			page = split2[0]
 			section = split2[1]
-		} else if len(split2) > 2 {
+		} else {
 			return false, []string{}, errors.New("internal link target must not contain multiple hashes")
 		}
 
-		// site:page
-		split3 := strings.Split(split2[0], ":")
-		if len(split3) == 1 {
-			page = split3[0]
-		} else if len(split3) == 2 {
-			site = split3[0]
-			page = split3[1]
-		} else {
-			return false, []string{}, errors.New("internal link target must not contain multiple colons")
-		}
-
-		site = strings.ToLower(site)
-		page = strings.ReplaceAll(strings.ToLower(page), " ", "_")
-		section = strings.ReplaceAll(strings.ToLower(section), " ", "_")
-		return false, []string{display, site, page, section}, nil
+		return false, []string{display, page, section}, nil
 	}
 }
