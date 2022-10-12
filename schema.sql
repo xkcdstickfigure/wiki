48595e806e08f3daa2b96a058b19cdd6e33b0430
zero <zero@alles.cx>
2022-10-12 20:15:58 +0100

    removed "not null" from sql schema

--- schema.sql
+++ schema.sql
@@ -1,15 +1,15 @@
 create table site (
     id               uuid   primary key,
-    name             text   unique not null,
-    display_name     text   not null,
+    name             text   unique,
+    display_name     text,
     discord_server   text
 );
 
 create table article (
     id        uuid   primary key,
-    site_id   uuid   references site on delete cascade not null,
-    slug      text   not null,
-    title     text   not null,
-    source    text   not null,
+    site_id   uuid   references site on delete cascade,
+    slug      text,
+    title     text,
+    source    text,
     unique(site_id, slug)
 );
