b4cfdbc6a7552b008923b8eb7018b0a874ace220
zero <zero@alles.cx>
2022-10-12 20:52:25 +0100

    removed bad code

--- store/Article.go
+++ store/Article.go
@@ -15,9 +15,5 @@ type Article struct {
 func (s Store) ArticleGetBySlug(ctx context.Context, siteId string, slug string) (Article, error) {
 	var article Article
 	err := s.Conn.QueryRow(ctx, "select id, site_id, slug, title, source from article where site_id=$1 and slug=$2", siteId, slug).Scan(&article.Id, &article.SiteId, &article.Slug, &article.Title, &article.Source)
-	if err != nil {
-		return article, err
-	} else {
-		return article, nil
-	}
+	return article, err
 }
--- store/Site.go
+++ store/Site.go
@@ -15,9 +15,5 @@ type Site struct {
 func (s Store) SiteGetByName(ctx context.Context, name string) (Site, error) {
 	var site Site
 	err := s.Conn.QueryRow(ctx, "select id, name, display_name, discord_server from site where name=$1", name).Scan(&site.Id, &site.Name, &site.DisplayName, &site.DiscordServer)
-	if err != nil {
-		return site, err
-	} else {
-		return site, nil
-	}
+	return site, err
 }
