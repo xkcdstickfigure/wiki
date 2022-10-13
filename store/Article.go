c427f3e72b8dca07604906cb5ad6500ee2a93d03
zero <zero@alles.cx>
2022-10-13 22:40:03 +0100

    formatting

--- store/Article.go
+++ store/Article.go
@@ -14,6 +14,7 @@ type Article struct {
 
 func (s Store) ArticleGetBySlug(ctx context.Context, siteId string, slug string) (Article, error) {
 	var article Article
-	err := s.Conn.QueryRow(ctx, "select id, site_id, slug, title, source from article where site_id=$1 and slug=$2", siteId, slug).Scan(&article.Id, &article.SiteId, &article.Slug, &article.Title, &article.Source)
+	err := s.Conn.QueryRow(ctx, "select id, site_id, slug, title, source from article where site_id=$1 and slug=$2", siteId, slug).
+		Scan(&article.Id, &article.SiteId, &article.Slug, &article.Title, &article.Source)
 	return article, err
 }
--- store/Site.go
+++ store/Site.go
@@ -14,6 +14,7 @@ type Site struct {
 
 func (s Store) SiteGetByName(ctx context.Context, name string) (Site, error) {
 	var site Site
-	err := s.Conn.QueryRow(ctx, "select id, name, display_name, discord_server from site where name=$1", name).Scan(&site.Id, &site.Name, &site.DisplayName, &site.DiscordServer)
+	err := s.Conn.QueryRow(ctx, "select id, name, display_name, discord_server from site where name=$1", name).
+		Scan(&site.Id, &site.Name, &site.DisplayName, &site.DiscordServer)
 	return site, err
 }
