d5312f2fb3f4f9f2e0c5f14632362d8f683b76bc
zero <zero@alles.cx>
2022-10-17 15:36:17 +0100

    better edit link

--- render/header.go
+++ render/header.go
@@ -5,18 +5,14 @@ import (
 )
 
 func renderHeader(pctx PageContext) string {
-	output := `<div class="flex justify-between space-x-2">`
-	output += `<div class="min-w-0">`
-	output += `<h1 class="text-4xl font-semibold">`
+	output := `<h1 class="text-4xl font-semibold">`
 	output += html.EscapeString(pctx.Title)
 	output += `</h1>`
-	output += `<h2 class="text-sm text-gray-600 whitespace-pre overflow-hidden text-ellipsis">`
+	output += `<h2 class="text-sm flex space-x-1">`
+	output += `<span class="text-gray-600 whitespace-pre overflow-hidden text-ellipsis">`
 	output += html.EscapeString(pctx.Site + "." + pctx.Domain + "/" + pctx.Slug)
+	output += `</span>`
+	output += `<a class="text-blue-700 hover:underline" href="/` + html.EscapeString(pctx.Slug) + `/edit">[edit]</a>`
 	output += `</h2>`
-	output += `</div>`
-	output += `<div>`
-	output += `<a class="block py-1 px-2 uppercase text-xs text-gray-600 font-semibold bg-gray-100 border border-gray-200" href="/` + html.EscapeString(pctx.Slug) + `/edit">Edit</a>`
-	output += `</div>`
-	output += `</div>`
 	return output
 }
